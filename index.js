f811aaccff52b9864092def48d5cc7694f21316d
thiago <viertelthiago@gmail.com>
2026-07-06 22:17:38 -0300

    ok

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 import express from 'express'
+import bancoDeDados from './repository/index.js'    
 
 const app = express()
-//configura uma rota para metodo http get na raiz ("/") do aplicativo
-app.get("/api/v1/somar", (req, res) => {
-    const num1 = 10 
-    const num2 = 20
-    const resultado = num1 + num2
-    //retorna uma resposta json contendo uma mensagem "Hello World"
-    res.send({ resultado })
-    console.log(num1,num2)
+app.get("/api/pessoa/:id", (req, res) => {
+    const id = req.params.id
+    const pessoa =bancoDeDados.find(it => it.id == id)
+    if (!pessoa) {
+        res.send({message: "Pessoa não encontrada"})
+    }
+
+    res.send({ pessoa })
 })
+
+app.get("/api/pessoa", (req, res) => {
+    const {id, name} = req.query
+
+    if(!id || !name) {
+        res.send({ message: "Favor informar id e name"})
+        return
+    }
+    bancoDeDados.push({id, name })
+    console.log(bancoDeDados)
+    res.send({message: "Pessoa criada com sucesso"})
+})
+
 // Inicia o servidor Express na porta 3000 e define uma função de retorno de
 app.listen(3000, () => {
     //Exibe uma mensagem no
-    console.log("Servidor ouvinda na porta 3000")
+    console.log("Servidor escutando na porta 3000")
 })
+
+//buscar - get - /api/pessoa/:id
+//criar - get - /api/pessoa
+//alterar - get - /api/alterar/:id
+//deletar - get - /spi/deletar/:id
+//buscarT - get - /api/buscar <-------------->
---
+++ repository/index.js
@@ -0,0 +1,3 @@
+const bancoDeDados = []
+
+export default bancoDeDados
