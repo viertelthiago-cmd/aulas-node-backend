26bb8f617aa20bdd4b6e66e86b277af04d7dfb51
thiago <viertelthiago@gmail.com>
2026-07-08 20:17:12 -0300

    ok

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 import express from 'express'
-import bancoDeDados from './repository/index.js'    
-
+    
 const app = express()
-app.get("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
-    const pessoa =bancoDeDados.find(it => it.id == id)
-    if (!pessoa) {
-        res.send({message: "Pessoa não encontrada"})
-    }
 
-    res.send({ pessoa })
+app.get("/api/v1/somar", (req, res) => {
+    const {num1, num2} = req.query
+    const resultado = Number(num1) + Number(num2)
+
+    res.status(200).send({message: resultado })
 })
 
-app.get("/api/pessoa", (req, res) => {
-    const {id, name} = req.query
 
-    if(!id || !name) {
-        res.send({ message: "Favor informar id e name"})
-        return
-    }
-    bancoDeDados.push({id, name })
-    console.log(bancoDeDados)
-    res.send({message: "Pessoa criada com sucesso"})
-})
+
+
+
 
 // Inicia o servidor Express na porta 3000 e define uma função de retorno de
 app.listen(3000, () => {
--- repository/index.js
+++
@@ -1,3 +0,0 @@
-const bancoDeDados = []
-
-export default bancoDeDados
