8098c7af2a338ccbf627706eff9173c7caa21c23
thiago <viertelthiago@gmail.com>
2026-08-03 22:01:43 -0300

    ok

--- index.js
+++ index.js
@@ -1,39 +1,10 @@
 import express from 'express'
-import router from './router/atividade1'
-import router from './router/atividade2'
+import cachorro from './router/cachorro.js'
 const app = express()
 
 app.use(express.json())
 
-app.get("/api/v1/somar", (req, res) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) + Number(num2)
-    res.send({ resultado })
-})
-
-app.use('/api/v1/atvd1', atvd1)
-
-app.post("/api/v1/atvd2", (req, res) => {
-    const { num1, num2, num3, num4, num5 } = req.body
-    const resultado = num1 + num2 + num3 + num4 + num5
-    res.send({ resultado })
-
-})
-
-app.post("/api/v1/atvd3", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = num1 + num2
-    res.send({ resultado })
-
-})
-
-app.post("/api/v1/atvd4", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = num1 + num2
-    res.send({ resultado })
-
-})
-
+app.use('/api/v1/cachorro', cachorro)
 
 
 // Inicia o servidor Express na porta 3000 e define uma função de retorno de
@@ -41,9 +12,3 @@ app.listen(3000, () => {
     //Exibe uma mensagem no
     console.log("Servidor escutando na porta 3000")
 })
-
-//buscar - get - /api/pessoa/:id
-//criar - get - /api/pessoa
-//alterar - get - /api/alterar/:id
-//deletar - get - /spi/deletar/:id
-//buscarT - get - /api/buscar <-------------->
---
+++ repository/cachorro.js
@@ -0,0 +1,3 @@
+const cachorro = []
+
+export default cachorro;
